4f731957352be09b9947a7ed8674e30d5c41dec5
Muhammed Bilal <muhammedbilal6211@gmail.com>
2024-07-11 20:51:16 +0530

    added icon and meta

--- js/script.js
+++ js/script.js
@@ -104,3 +104,13 @@ document.getElementById('whtsp-btn').addEventListener('click', function () {
 
     window.location.href = whatsappUrl;
 });
+
+
+document.getElementById('whatsappButton').addEventListener('click', function () {
+    const phoneNumber = '+917510102069'; // Replace with the target phone number
+    const message = 'Hello! I am interested in your construction services and would like to discuss my project with you.';
+    // Basic message for a construction company
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
+    window.location.href = whatsappUrl;
+});
