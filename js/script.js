a19d2323de32b87ca4dc273506d5b5f1c9f40264
Muhammed Bilal <muhammedbilal6211@gmail.com>
2024-07-16 00:17:17 +0530

    added whatsap defualt message for booking and turf banner height for small devices

--- js/script.js
+++ js/script.js
@@ -115,7 +115,7 @@ MshandleScroll();
 
 document.getElementById('booking-btn').addEventListener('click', function () {
     const phoneNumber = '+917510102069'; // Replace with the target phone number
-    const message = 'Hello! I am interested in booking a slot for FootBee turf. Please provide the available slots and details.';
+    const message = 'Hello! I am interested in booking a slot for Netz turf. Please provide the available slots and details.';
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
 
     alert('You are being redirected to WhatsApp to book your slot. Please confirm your booking there.');
